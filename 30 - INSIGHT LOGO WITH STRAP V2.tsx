d89c7073e3bcadb1c17ff1b1665b83717183e6ae
matster75 <38755750+matster75@users.noreply.github.com>
2020-10-02 13:33:32 +0100

    New map

---
+++ 30 - INSIGHT LOGO WITH STRAP V2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="30 - INSIGHT LOGO WITH STRAP V2" tilewidth="33" tileheight="33" tilecount="51" columns="17">
+ <image source="30 - INSIGHT LOGO WITH STRAP V2.jpg" width="561" height="123"/>
+</tileset>
